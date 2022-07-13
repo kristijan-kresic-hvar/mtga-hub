405e3b8959c5f7dd3cced10a851e6be82e2c136b
Kristijan Krešić <52584740+kristijan-kresic-hvar@users.noreply.github.com>
2022-07-13 02:31:56 +0200

    Setup infinite loading && scroll back to top button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { useStateContext } from '../contexts/ContextProvider';
 
-const Card = (props) => {
+const Card = forwardRef((props, ref) => {
     const { setCardPopupOpen, setSelectedCard, currentMode } = useStateContext()
 
     const handleCardClick = (card) => {
@@ -18,11 +18,11 @@ const Card = (props) => {
 
     return (
         <>
-            <div style={{ borderRadius: '1.1rem' }} onClick={() => handleCardClick(props?.cardValues)} className="select-none transition-all ease-in-out duration-200ms border-transparent border-[3px] hover:border-[#3416DE] dark:hover:border-orange-600 w-4/5 hover:shadow-md hover:shadow-black cursor-pointer hover:scale-105 ml-auto mr-auto">
+            <div ref={ref && ref} style={{ borderRadius: '1.1rem' }} onClick={() => handleCardClick(props?.cardValues)} className="select-none transition-all ease-in-out duration-200ms border-transparent border-[3px] hover:border-[#3416DE] dark:hover:border-orange-600 w-4/5 hover:shadow-md hover:shadow-black cursor-pointer hover:scale-105 ml-auto mr-auto">
                 <img style={{ borderRadius: '1.1rem' }} title={props?.cardValues?.name} src={(props?.cardValues?.imageUrl || props?.cardValues?.image_uris?.normal || props?.cardValues?.card_faces?.[0]?.image_uris?.normal) || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWxmQH85oJQYthg6KgnjmCX3ty-PPZkn-rHg&usqp=CAU"} alt="card thumbnail" className="w-full object-contain h-full" />
             </div>
         </>
     )
-}
+})
 
 export default Card
--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 // tslint:disable:max-line-length
 import { useStateContext } from '../contexts/ContextProvider';
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
@@ -7,6 +7,7 @@ import Card from '../components/Card'
 import { useParams } from 'react-router-dom'
 import { getAllCards, getAllCardsInSet, getSingleSet } from '../api'
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import { useNavigate } from 'react-router-dom'
 
 const CardList = () => {
 
@@ -14,73 +15,70 @@ const CardList = () => {
 
     const { setId } = useParams()
 
+    const location = useNavigate()
+
     const [cards, setCards] = useState([])
     const [totalCards, setTotalCards] = useState()
-    const [resultsPerPage, setResultsPerPage] = useState(50)
+    const [resultsPerPage, setResultsPerPage] = useState()
     const [currentPage, setCurrentPage] = useState(1)
-    const [currentPageInputValue, setCurrentPageInputValue] = useState(1)
     const [noData, setNoData] = useState(false)
     const [cardPreloadersCount] = useState([...Array(100)])
-    const [currentSetId, setCurrentSetId] = useState()
+    const [currentSetId, setCurrentSetId] = useState("all")
     const [windowSize, setWindowSize] = useState(window.innerWidth)
+    const [hasMore, setHasMore] = useState(false)
 
-    const firstPage = () => {
-        setCurrentPage(1)
-    }
+    const observer = useRef()
+    const lastCardElementRef = useCallback(node => {
+        if (loading || !cards.length > 0) return
+        if (observer?.current) observer?.current?.disconnect()
 
-    const lastPage = () => {
-        setCurrentPage(Math.ceil(totalCards / resultsPerPage))
-    }
+        observer.current = new IntersectionObserver(entries => {
+            if (!loading && cards?.length > 0 && entries[0].isIntersecting && (hasMore || !(currentPage >= (Math.ceil(totalCards / resultsPerPage))))) {
+                if (hasMore || !(currentPage >= (Math.ceil(totalCards / resultsPerPage)))) {
+                    setCurrentPage(prevPage => prevPage + 1)
+                }
 
-    const nextPage = () => {
-        setCurrentPage(prevPage => prevPage + 1)
-    }
+                return false
+            }
+        })
 
-    const previousPage = () => {
-        setCurrentPage(prevPage => prevPage - 1)
-    }
+        if (node && cards?.length > 0) observer.current.observe(node)
+
+        console.log(node)
+    }, [cards, loading, hasMore])
 
     const handleWindowResize = () => {
         setWindowSize(window.innerWidth)
     }
 
-    const handlePageInputChange = (e) => {
-        if (!e.target.value || Number(e.target.value) > (Math.ceil(totalCards / resultsPerPage)) || Number(e.target.value) <= 0) return false
-        setCurrentPageInputValue(Number(e.target.value))
-
-        setTimeout(() => {
-            setCurrentPage(Number(e.target.value))
-        }, 1500)
-    }
-
     useEffect(() => {
-        setCards([])
         setNoData(false)
-
-        currentSetId === setId ? currentPage : setCurrentPage(1)
+        setLoading(true)
 
         setTimeout(() => {
-
             if (!setId) {
-                setResultsPerPage(50)
-                getAllCards(currentPage, resultsPerPage)
-                    .then(data => {
 
+                setResultsPerPage(50)
+                setCurrentSetId("all")
+
+                return getAllCards(currentSetId === "all" ? currentPage : 1, resultsPerPage)
+                    .then(data => {
                         if (!data) {
                             setLoading(false)
                             return setNoData(true)
                         }
 
-                        setCards(data?.cards)
-                        setTotalCards(data?.total)
+                        if (currentPage === 1) {
+                            setCards(data?.cards)
+                            setTotalCards(data?.total)
+                            setLoading(false)
+                        } else {
+                            setCards(prevCards => [...prevCards, ...data?.cards])
+                            setTotalCards(data?.total)
+                            setLoading(false)
+                        }
                     })
-                    .then(() => setLoading(false))
                     .catch(error => console.log(error))
-
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                })
             } else {
                 setResultsPerPage(175)
                 setCurrentSetId(setId)
@@ -90,26 +88,39 @@ const CardList = () => {
                         if (data) {
                             getAllCardsInSet(data?.search_uri, currentSetId === setId ? currentPage : 1)
                                 .then(data => {
-
                                     if (!data) {
                                         setLoading(false)
                                         return setNoData(true)
                                     }
 
-                                    setCards(data?.data)
-                                    setTotalCards(data?.total_cards)
+                                    if (currentPage === 1) {
+                                        setCards(data?.data)
+                                        setHasMore(data?.has_more)
+                                        setTotalCards(data?.total_cards)
+
+                                        setLoading(false)
+                                    } else {
+                                        setCards(prevCards => [...prevCards, ...data?.data])
+                                        setHasMore(data?.has_more)
+                                        setTotalCards(data?.total_cards)
+
+                                        setLoading(false)
+                                    }
                                 })
                         }
                     })
                     .catch(error => console.log(error.message))
-
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                })
             }
         }, 2500)
     }, [currentPage, setId])
+
+    useEffect(() => {
+        if ((currentSetId !== setId)) {
+            window.scrollTo(0, 0)
+            setCurrentPage(1)
+            setCards([])
+        }
+    }, [setId])
 
     useEffect(() => {
         window.addEventListener('resize', handleWindowResize)
@@ -129,64 +140,16 @@ const CardList = () => {
                         </div>
                     ))
                 }
-                {cards?.length > 0 && cards?.map((card, index) => (
-                    <Card key={index} cardValues={card} />
-                ))}
+                {cards?.length > 0 && cards?.map((card, index) => {
+                    if (cards?.length === index + 1) {
+                        return <Card key={index} ref={lastCardElementRef} cardValues={card} />
+                    } else {
+                        return <Card key={index} cardValues={card} />
+                    }
+                })}
                 {!cards?.length > 0 && !loading && noData && <h3 className="dark:text-white">Nothing to show...</h3>}
             </div>
-            {
-                cards?.length > 0 && !loading &&
-                <div className="text-center mt-10">
-                    {windowSize > 810 &&
-                        <div>
-                            <ButtonComponent disabled={currentPage === 1} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-normal mr-10`} onClick={firstPage}>First</ButtonComponent>
-                            <ButtonComponent disabled={currentPage === 1} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-large`} onClick={previousPage}>Previous Page</ButtonComponent>
-                            <span className="ml-5 mr-5 dark:text-white">Page <input onChange={handlePageInputChange} className="w-20 ml-3 text-black" type="number" value={currentPageInputValue} /> of {totalCards && (Math.ceil(totalCards / resultsPerPage))}</span>
-                            <ButtonComponent disabled={currentPage === (Math.ceil(totalCards / resultsPerPage))} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-large`} onClick={nextPage}>Next Page</ButtonComponent>
-                            <ButtonComponent disabled={currentPage === (Math.ceil(totalCards / resultsPerPage))} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-normal ml-10`} onClick={lastPage}>Last</ButtonComponent>
-                        </div>
-                    }
-
-                    {windowSize < 810 &&
-                        <div style={{ maxWidth: '400px' }} className="flex ml-auto mr-auto items-center justify-center">
-                            <div className="flex flex-col justify-center ml-2">
-                                <div>
-                                    <ButtonComponent disabled={currentPage === 1} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-large`} onClick={previousPage}>{window.innerWidth < 810 ? 'Previous' : 'Previous Page'}</ButtonComponent>
-                                </div>
-                                <div>
-                                    <ButtonComponent disabled={currentPage === 1} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-normal mt-3`} onClick={firstPage}>First</ButtonComponent>
-                                </div>
-                            </div>
-
-                            <div className="dark:text-white inline-block text-center ml-auto mr-auto">
-                                <div className="mb-2">
-                                    <small>Page </small>
-                                </div>
-                                <div>
-                                    <input onChange={handlePageInputChange} className="w-20 ml-3 text-black" type="number" value={currentPageInputValue} />
-                                </div>
-                                <div className="mt-2">
-                                    <span>of</span> {totalCards && (Math.ceil(totalCards / resultsPerPage))}
-                                </div>
-                            </div>
-
-                            <div className="ml-auto mr-2 flex items-center justify-center">
-                                <div className="flex flex-col justify-center">
-                                    <div>
-                                        <ButtonComponent disabled={currentPage === (Math.ceil(totalCards / resultsPerPage))} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-large`} onClick={nextPage}>{window.innerWidth < 810 ? 'Next' : 'Next Page'}</ButtonComponent>
-                                    </div>
-                                    <div>
-                                        <ButtonComponent disabled={currentPage === (Math.ceil(totalCards / resultsPerPage))} cssClass={`${currentMode === 'Dark' ? 'e-warning' : 'e-info'} e-normal mt-3`} onClick={lastPage}>Last</ButtonComponent>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    }
-                </div>
-            }
         </div >
-
     );
 }
 
